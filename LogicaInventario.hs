762789da4d6ec3c8361ea3b88a9749b7516e329c
Matheus Yamamoto Dias <matheusyamas@gmail.com>
2025-11-11 17:04:57 -0300

    v1 - LogicaInventario implementada

---
+++ LogicaInventario.hs
@@ -0,0 +1,67 @@
+module LogicaInventario where
+import DadosInventario
+-- Importa função para manipular o map
+import qualified Data.Map as Map
+import Data.Time (UTCTime)
+
+type ResultadoOperacao = (Inventario, LogEntry)
+
+
+addItem :: UTCTime -> Item -> Inventario -> Either String ResultadoOperacao
+addItem timestamp novoItem inventario = 
+    let idDoItem = itemID novoItem
+    
+    in if Map.member idDoItem inventario
+        then
+            Left "Erro: ID do item já existe."
+        else
+            let novoInventario = Map.insert idDoItem novoItem inventario
+            
+                logEntry = LogEntry
+                    { timestamp = timestamp
+                    , acao      = Add
+                    , detalhes  = "Item '" ++ (nome novoItem) ++ "' adicionado."
+                    , status    = Sucesso
+                    }
+            in Right (novoInventario, logEntry)
+            
+            
+removeItem :: UTCTime -> String -> Int -> Inventario -> Either String ResultadoOperacao
+removeItem timestamp itemID qtdRemover inventario =
+    let resultadoBusca = Map.lookup itemID inventario 
+    in case resultadoBusca of
+        Nothing  ->
+            Left "Erro: Item não encontrado."
+        Just itemEncontrado ->
+            if(quantidade itemEncontrado) < qtdRemover
+            then
+                Left "Erro: Estoque insuficiente."
+            else
+                let novaQtd = (quantidade itemEncontrado) - qtdRemover
+                    itemAtualizado = itemEncontrado { quantidade = novaQtd }
+                    novoInventario = Map.insert itemID itemAtualizado inventario
+                    logEntry = LogEntry
+                        { timestamp = timestamp
+                        , acao      = Remove
+                        , detalhes  = "Removidas " ++ (show qtdRemover) ++ " unidades do item '" ++ (nome itemEncontrado) ++ "'."
+                        , status    = Sucesso
+                        }
+                in Right (novoInventario, logEntry)
+                
+updateQty :: UTCTime -> String -> Int -> Inventario -> Either String ResultadoOperacao
+updateQty timestamp itemID qtdAdicionar inventario =
+    let resultadoBusca = Map.lookup itemID inventario
+    in case resultadoBusca of
+        Nothing   -> 
+            Left "Erro: Item não encontrado."
+        Just itemEncontrado ->
+            let novaQtd = (quantidade itemEncontrado) + qtdAdicionar
+                itemAtualizado = itemEncontrado { quantidade = novaQtd }
+                novoInventario = Map.insert itemID itemAtualizado inventario
+                logEntry = LogEntry
+                    { timestamp = timestamp
+                    , acao      = Update
+                    , detalhes  = "Adicionadas " ++ (show qtdAdicionar) ++ " unidades do item '" ++ (nome itemEncontrado) ++ "'."
+                    , status    = Sucesso
+                    }
+            in Right (novoInventario, logEntry)
